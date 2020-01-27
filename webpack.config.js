e1df249915f87219783cfe88e3302c4a11e51830
DaryaMarkova <Darya.Markova@dataart.com>
2020-01-27 14:56:40 +0300

    applying jade template

--- webpack.config.js
+++ webpack.config.js
@@ -27,15 +27,11 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        use: ['file-loader']
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
+        use: ['file-loader']
       },
       {
         test: /\.m?js$/,
@@ -46,12 +42,16 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.pug$/,
+        use: ['pug-loader']
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'index.html')
+      template: path.resolve(__dirname, 'index.pug')
     }),
     new CopyPlugin([
       { from: './src/assets', to: path.resolve(__dirname, 'build/assets')}
