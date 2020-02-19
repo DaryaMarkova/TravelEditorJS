a895e4f4ca65480b715d6a40301042ee81cb01c1
darya-markova <darya.towarddreams@gmail.com>
2020-02-20 00:40:27 +0300

    draft user interface

--- src/index.js
+++ src/index.js
@@ -9,11 +9,10 @@ import { OSM } from 'ol/source';
 import { Tile as TileLayer } from 'ol/layer';
 import { ContextMenuControl } from './controls/map.context.menu';
 import { MapCountryEditor } from './editors/country-editor/country.editor';
-import { defaults as defaultControls } from 'ol/control';
+import { defaults as defaultControls, Control } from 'ol/control';
 import { MapControlPanel } from './controls/map.control.panel';
 import { MapZoomControl } from './controls/map.zoom';
 import { MapMarkerEditor } from './editors/marker-editor/marker.editor';
-
 export const map = new Map({
   layers: [
     new TileLayer({
@@ -28,7 +27,9 @@ export const map = new Map({
   }),
   controls: defaultControls().extend([
     new MapControlPanel(this, '#control-panel'),
-    new MapZoomControl(this, '#map-zoomer'),
+		new MapZoomControl(this, '#map-zoomer'),
+		new Control({element: document.querySelector('#app-header')}),
+		new Control({element: document.querySelector('#notification')})
   ])
 });
 
